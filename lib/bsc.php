8ec5396687e72e715d332625ac4d05e9a528710b
Mike Thorn <mthorn@bahamut.(none)>
2013-04-08 09:59:38 -0400

    adding bsd license

--- lib/bsc.php
+++ lib/bsc.php
@@ -1,4 +1,7 @@
 <?php
+# Copyright 2013 Mike Thorn (github: WasabiVengeance). All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 global $__bsc_widget_search_paths;
 $__bsc_widget_search_paths = array(
--- lib/widgets/button_toolbar.php
+++ lib/widgets/button_toolbar.php
@@ -1,4 +1,7 @@
 <?php
+# Copyright 2013 Mike Thorn (github: WasabiVengeance). All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 
 class bsc_widget_button_toolbar extends bsc_widget
 {
